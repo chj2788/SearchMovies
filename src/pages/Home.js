4804c3ea1959ff864f09ce48a7887e6c6b900bf8
chj2788 <chj2788@gmail.com>
2021-01-26 15:53:07 +0900

    created input field and fetched results from api

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,41 @@
 /* eslint-disable */
-import React from 'react';
+import React, { useState } from 'react';
 import MainPageLayout from '../components/MainPageLayout';
 
 const Home = () => {
-  return <MainPageLayout>this is home</MainPageLayout>;
+  const [input, setInput] = useState('');
+
+  const onInputChange = event => {
+    setInput(event.target.value);
+  };
+
+  const onSearch = () => {
+    fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
+      .then(response => response.json())
+      .then(result => {
+        console.log(result);
+      });
+  };
+
+  const onKeyDown = event => {
+    if (event.keyCode === 13) {
+      onSearch();
+    }
+  };
+
+  return (
+    <MainPageLayout>
+      <input
+        type="text"
+        onChange={onInputChange}
+        onKeyDown={onKeyDown}
+        value={input}
+      />
+      <button type="button" onClick={onSearch}>
+        Search
+      </button>
+    </MainPageLayout>
+  );
 };
 
 export default Home;
